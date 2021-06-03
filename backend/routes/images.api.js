5a2224fe47973d10c9114e86a14748671fc0a0cf
unknown <everythinginmoderation2@gmail.com>
2021-06-03 18:44:51 +0700

    frontend only

--- backend/routes/images.api.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var router = express.Router()
-const memeController = require('../controllers/meme.controller')
-
-/* GET all images for images-only page at (domain/images) */
-router.get('/',  memeController.getOriginalImages);
-
-module.exports = router
